df2d876510f7f3e44c5161ec123d320c27498d92
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-11-17 17:53:28 -0300

    Requisito 4

--- src/main.js
+++ src/main.js
@@ -5,8 +5,23 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const addLoading = () => {
+  const loadEl = document.createElement('div');
+  loadEl.className = 'loading';
+  loadEl.innerHTML = 'carregando...';
+  const body = document.querySelector('body');
+  body.appendChild(loadEl);
+};
+
+const removeLoading = () => {
+  const loadEl = document.querySelector('.loading');
+  loadEl.remove();
+};
+
 const createListOfProducts = async () => {
+  addLoading();
   const data = await fetchProductsList('computador');
+  removeLoading();
   data.forEach((element) => {
     const { id, title, thumbnail, price } = element;
     const obj = { id, title, thumbnail, price };
@@ -18,3 +33,10 @@ const createListOfProducts = async () => {
 };
 
 createListOfProducts();
+
+// Sugestão: Você pode criar uma função que adicione ao DOM o elemento com o texto carregando... e outra para retirá-lo, o que acha?
+
+// O que será testado:
+
+// Verifica se o texto de "carregando" está visível durante uma requisição à API.
+// Verifica se o texto de "carregando" não está visível após a requisição à API ser concluída.
