92b6dd12f3b31643187cfc026da37dbc8a25600c
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-11-17 17:29:56 -0300

    Requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const createListOfProducts = async () => {
+  const data = await fetchProductsList('computador');
+  data.forEach((element) => {
+    const { id, title, thumbnail, price } = element;
+    const obj = { id, title, thumbnail, price };
+    const product = createProductElement(obj);
+    const productSection = document.querySelector('.products');
+    productSection.appendChild(product);
+    return product;
+  });
+};
+
+createListOfProducts();
